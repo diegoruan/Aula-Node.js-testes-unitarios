1ee6224c061c6340a10483818547d7284ceb51c2
Diego Ruan <diego_ruan.14@hotmail.com>
2024-04-21 11:56:23 -0300

    Asserções e Jest - Aula 2

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtras;
 
-console.log(somaHorasExtras(2000, 500));
+const calculaDescontos = (salario, descontos) => salario - descontos;
+
+export { somaHorasExtras, calculaDescontos };
